4af145f48b7300f68e68aa58c3ada5fdfbb677e1
Joel Mitchell <2780344+HeyJoel@users.noreply.github.com>
2022-08-01 10:12:57 +0100

    Migrated to .NET6

--- src/ImageSharpExample/Program.cs
+++ src/ImageSharpExample/Program.cs
@@ -1,26 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Cofoundry.Web;
 
-namespace ImageSharpExample
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Services
+    .AddControllersWithViews()
+    .AddCofoundry(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCofoundry();
+
+app.Run();
